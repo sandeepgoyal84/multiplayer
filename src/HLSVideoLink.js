bc4cb55fc8aaee741434f33c42f3f1f56b0553b2
sandeepgoyal84 <goyal.sandeep1984@gmail.com>
2023-03-24 13:11:27 +0530

    Update HLSVideoLink.js

--- src/HLSVideoLink.js
+++ src/HLSVideoLink.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 function HLSVideoLink({ projectVideo, video }) {
     return (
-        <div style={{ color: "white", padding: "15px 5px", wordBreak: 'break-all' }}>
-            <a href="#" onClick={() => projectVideo(video)}>
+        <div style={{ display: "flex", color: "white", padding: "15px 10px", wordBreak: 'break-all' }}>
+            <div style={{ flexGrow: 1 }} onClick={() => projectVideo(video)}>
                 {video.title}
-            </a>
+            </div>
+            <div style={{backgroundColor: `rgb(50, 5, 5)`, }} onClick={() => projectVideo(video)}>
+                Refresh
+            </div>
         </div>
     )
 }
