9be6075e76f2121be91692e42c5c2bffb502cd0a
sandeep <goyal.sandeep1984@gmail.com>
2023-03-24 20:14:58 +0530

    refresh button

--- src/HLSComponent.js
+++ src/HLSComponent.js
@@ -10,27 +10,27 @@ const videoData = [{
     "title": "BigBuckBunny.mp4"
 },
 {
-    "src": "https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    "src": "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8",
     "poster": "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
     "title": "ElephantsDream.mp4"
 },
 {
-    "src": "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    "src": "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8",
     "poster": "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
     "title": "ForBiggerBlazes.mp4"
 },
 {
-    "src": "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    "src": "https://moctobpltc-i.akamaihd.net/hls/live/571329/eight/playlist.m3u8",
     "poster": "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerEscapes.jpg",
     "title": "ForBiggerEscapes.mp4"
 },
 {
-    "src": "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+    "src": "http://d3rlna7iyyu8wu.cloudfront.net/skip_armstrong/skip_armstrong_stereo_subs.m3u8",
     "poster": "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerFun.jpg",
     "title": "ForBiggerFun.mp4"
 },
 {
-    "src": "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    "src": "http://d3rlna7iyyu8wu.cloudfront.net/skip_armstrong/skip_armstrong_multi_language_subs.m3u8",
     "poster": "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerJoyrides.jpg",
     "title": "ForBiggerJoyrides.mp4"
 },
@@ -77,9 +77,14 @@ function HLSComponent() {
     const projectVideo = useCallback((video) => {
         if (!playingVideos.some(ele => ele.id === video.id)) {
             setPlayingVideos(oldState => [...oldState, video]);
+            setSelectedId({ "id": video.id });
         }
-        setSelectedId({ "id": video.id });
     }, [playingVideos]);
+
+    const restartVideo = useCallback((video) => {        
+        setSelectedId({ "id": video.id });
+    },[]);
+
     const handleAddVideo = () => {
         projectVideo({ "src": addVideo, "title": addVideo, "poster": "", id: addVideo });
         setAddVideo('');
@@ -89,11 +94,11 @@ function HLSComponent() {
         setPlayingVideos(filteredArray);
     };
     return (
-        <div style={{ flexGrow: 1, display: "flex", margin: "20px", backgroundColor: "black" }}>
-            <div style={{ flexBasis: "400px", display: "flex", flexDirection: "column" }}>
+        <div style={{ flexGrow: 1, display: "flex", margin: "20px 110px", backgroundColor: "black" }}>
+            <div style={{ flexBasis: "400px", flexShrink: 0, display: "flex", flexDirection: "column" }}>
                 <div style={{ flexGrow: 1, display: "flex", margin: "10px", flexDirection: "column" }}>
                     {videoList.map((video) => (
-                        <HLSVideoLink key={video.id} projectVideo={projectVideo} video={video} />
+                        <HLSVideoLink key={video.id} projectVideo={projectVideo} video={video} restart={restartVideo} isRestartVisible={playingVideos.some(ele => ele.id === video.id)}/>
                     ))}</div>
                 <div style={{ display: "flex", flexDirection: "column", margin: "50px 5px 10px 5px", backgroundColor: `rgb(50, 5, 5)` }}>
                     <input className='input' style={{ margin: "10px" }} type='text' value={addVideo} placeholder="Enter video url" onChange={e => setAddVideo(e.target.value)} />
--- src/HLSVideoLink.js
+++ src/HLSVideoLink.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function HLSVideoLink({ projectVideo, video }) {
+function HLSVideoLink({ projectVideo, video, restart, isRestartVisible }) {
     return (
         <div style={{ display: "flex", color: "white", padding: "15px 10px", wordBreak: 'break-all' }}>
             <div style={{ flexGrow: 1 }} onClick={() => projectVideo(video)}>
                 {video.title}
             </div>
-            <div style={{backgroundColor: `rgb(50, 5, 5)`, }} onClick={() => projectVideo(video)}>
+            {isRestartVisible && (<div style={{ backgroundColor: `rgb(50, 5, 5)`, }} onClick={() => restart(video)}>
                 Refresh
-            </div>
+            </div>)}
         </div>
     )
 }
