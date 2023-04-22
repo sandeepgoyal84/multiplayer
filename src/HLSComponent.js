d668469fcacd4b9af448bfb3c269c88fe4f1e658
sandeep <goyal.sandeep1984@gmail.com>
2023-04-22 18:50:09 +0530

    multiple videos on viweport

--- src/HLSComponent.js
+++ src/HLSComponent.js
@@ -1,66 +1,111 @@
-import React, { useCallback, useState, useRef, useEffect } from 'react';
-import HLSVideoListWrapper from './HLSVideoListWrapper';
-import HLSVideoLink from './HLSVideoLink';
-import videoData from './videos.json';
-import './App.css';
+import React, { useCallback, useState, useRef, useEffect } from "react";
+import HLSVideoListWrapper from "./HLSVideoListWrapper";
+import HLSVideoLink from "./HLSVideoLink";
+import videoData from "./videos.json";
+import "./App.css";
 
 function HLSComponent() {
-    const [videoList, setVideoList] = useState([]);
-    const [selectedId, setSelectedId] = useState({});
+  const [videoList, setVideoList] = useState([]);
+  const [selectedId, setSelectedId] = useState({});
 
-    //load data
-    useEffect(() => {
-        // get videodata
-        let vdata = JSON.parse(JSON.stringify(videoData)).data;
+  //load data
+  useEffect(() => {
+    // get videodata
+    let vdata = JSON.parse(JSON.stringify(videoData)).data;
 
-        let videos = [];
-        // add additinal properties
-        vdata.forEach((video, index) => {
-            videos.push({ ...video, "id": "" + index });
-        })
-        // set to state variable
-        setVideoList(oldState => [...videos]);
-    }, []);
-    const [addVideo, setAddVideo] = useState('');
-    const [playingVideos, setPlayingVideos] = useState([]);
+    let videos = [];
+    // add additinal properties
+    vdata.forEach((video, index) => {
+      videos.push({ ...video, id: "" + index });
+    });
+    // set to state variable
+    setVideoList((oldState) => [...videos]);
+  }, []);
+  const [addVideo, setAddVideo] = useState("");
+  const [playingVideos, setPlayingVideos] = useState([]);
 
-    const projectVideo = useCallback((video) => {
-        if (!playingVideos.some(ele => ele.id === video.id)) {
-            setPlayingVideos(oldState => [...oldState, video]);
-            setSelectedId({ "id": video.id });
-        }
-    }, [playingVideos]);
+  const projectVideo = useCallback(
+    (video) => {
+      if (!playingVideos.some((ele) => ele.id === video.id)) {
+        setPlayingVideos((oldState) => [...oldState, video]);
+        setSelectedId({ id: video.id });
+      }
+    },
+    [playingVideos]
+  );
 
-    const restartVideo = useCallback((video) => {
-        setSelectedId({ "id": video.id });
-    }, []);
+  const restartVideo = useCallback((video) => {
+    setSelectedId({ id: video.id });
+  }, []);
 
-    const handleAddVideo = () => {
-        projectVideo({ "src": addVideo, "title": addVideo, "poster": "", id: addVideo });
-        setAddVideo('');
-    };
-    const onClose = (vid) => {
-        let filteredArray = playingVideos.filter(item => item.id !== vid.id);
-        setPlayingVideos(filteredArray);
-    };
-    return (
-        <div style={{ flexGrow: "1", display: "flex", justifyContent: 'space-evenly' }}>
-            <div style={{ flex: "0 1 400px", backgroundColor: "black", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-                <div style={{ flexGrow: "1", margin: "10px" }}>
-                    {videoList.map((video) => (
-                        <HLSVideoLink key={video.id} projectVideo={projectVideo} video={video} restart={restartVideo} isRestartVisible={playingVideos.some(ele => ele.id === video.id)} />
-                    ))}
-                </div>
-                <div style={{ display: "flex", flexDirection: "column", margin: "50px 5px 10px 5px", backgroundColor: `rgb(50, 5, 5)` }}>
-                    <input className='input' style={{ margin: "10px" }} type='text' value={addVideo} placeholder="Enter video url" onChange={e => setAddVideo(e.target.value)} />
-                    <button className='button' style={{ margin: "10px" }} onClick={handleAddVideo}>play custom video</button>
-                </div>
-            </div>
-            <div style={{ flex: "0 0 1212px", backgroundColor: "black" }}>
-                <HLSVideoListWrapper playingVideos={playingVideos} selectedId={selectedId} onClose={onClose} />
-            </div>
+  const handleAddVideo = () => {
+    projectVideo({ src: addVideo, title: addVideo, poster: "", id: addVideo });
+    setAddVideo("");
+  };
+  const onClose = (vid) => {
+    let filteredArray = playingVideos.filter((item) => item.id !== vid.id);
+    setPlayingVideos(filteredArray);
+  };
+  return (
+    <div
+      style={{ flexGrow: "1", display: "flex", justifyContent: "space-evenly" }}
+    >
+      <div
+        style={{
+          flex: "0 1 30px",
+          backgroundColor: "black",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <div style={{ flexGrow: "1", margin: "5px" }}>
+          {videoList.map((video) => (
+            <HLSVideoLink
+              key={video.id}
+              projectVideo={projectVideo}
+              video={video}
+              restart={restartVideo}
+              isRestartVisible={playingVideos.some(
+                (ele) => ele.id === video.id
+              )}
+            />
+          ))}
         </div>
-    );
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            margin: "50px 5px 5px 5px",
+            backgroundColor: `rgb(50, 5, 5)`,
+          }}
+        >
+          <input
+            className="input"
+            style={{ margin: "5px" }}
+            type="text"
+            value={addVideo}
+            placeholder="Enter video url"
+            onChange={(e) => setAddVideo(e.target.value)}
+          />
+          <button
+            className="button"
+            style={{ margin: "5px" }}
+            onClick={handleAddVideo}
+          >
+            play custom video
+          </button>
+        </div>
+      </div>
+      <div style={{ flex: "0 0 1632px", backgroundColor: "black" }}>
+        <HLSVideoListWrapper
+          playingVideos={playingVideos}
+          selectedId={selectedId}
+          onClose={onClose}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default HLSComponent;
--- src/HLSVideoPlayer.js
+++ src/HLSVideoPlayer.js
@@ -1,199 +1,229 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
-import './App.css';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import "./App.css";
 
 // https://www.tutorialspoint.com/how-to-setup-video-js-with-reactjs
-// and 
+// and
 // https://videojs.com/guides/react/
 function HLSVideoPlayer({ video, selectedId, onClose }) {
-    const videoRef = useRef(null);
-    const playerRef = useRef(null);
-    const canvasRef = useRef(null);
-    const [volume, setVolume] = useState(0);
-    const videoJsOptions = {
-        autoplay: true,
-        controls: true,
-        responsive: true,
-        fluid: false,
-        height: "180px",
-        width: "400px",
-        html5: { hls: { overrideNative: true } },
-        sources: [{
-            src: video.src,
-        }]
+  const [errorMsg, setErrorMsg] = useState(null);
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [volume, setVolume] = useState(0);
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: false,
+    height: "90px",
+    width: "200px",
+    html5: { hls: { overrideNative: true } },
+    sources: [
+      {
+        src: video.src,
+      },
+    ],
+  };
+
+  // useeffect seq no 1 -- set volume
+  // reflect slider value as per setted volume
+  useEffect(() => {
+    const player = playerRef.current;
+    if (player) {
+      player.volume(volume);
+    }
+  }, [volume]);
+
+  // useeffect seq no 2 -- start/restart
+  useEffect(() => {
+    // Make sure Video.js player is only initialized once
+    if (selectedId.id === video.id) {
+      if (!playerRef.current) {
+        // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+        constructPlayer();
+      } else {
+        disposePlayer();
+        constructPlayer();
+      }
+    }
+  }, [selectedId]);
+
+  // useeffect seq no 3 -- dispose
+  // Dispose the Video.js player when the functional component unmounts
+  useEffect(() => {
+    return () => {
+      disposePlayer();
     };
+  }, []);
 
-    // useeffect seq no 1 -- set volume
-    // reflect slider value as per setted volume
-    useEffect(() => {
-        const player = playerRef.current;
-        if (player) {
-            player.volume(volume);
-        }
-    }, [volume]);
+  const disposePlayer = () => {
+    const player = playerRef.current;
+    if (player && !player.isDisposed()) {
+      player.dispose();
+      playerRef.current = null;
+    }
+  };
+  const constructPlayer = () => {
+    const videoElement = document.createElement("video");
 
-    // useeffect seq no 2 -- start/restart
-    useEffect(() => {// Make sure Video.js player is only initialized once
-        if (selectedId.id === video.id) {
-            if (!playerRef.current) {
-                // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
-                constructPlayer();
-            }
-            else {
-                disposePlayer();
-                constructPlayer();
-            }
-        }
-    }, [selectedId]);
+    videoElement.classList.add("video-js");
+    videoElement.classList.add("vjs-big-playcentered");
+    videoRef.current.appendChild(videoElement);
+    videoElement.disablepictureinpicture = true;
 
+    const player = (playerRef.current = videojs(
+      videoElement,
+      videoJsOptions,
+      () => {
+        videojs.log("player is ready");
+        // You can handle player events here, for example:
 
-    // useeffect seq no 3 -- dispose
-    // Dispose the Video.js player when the functional component unmounts
-    useEffect(() => {
-        return () => {
-            disposePlayer();
-        }
-    }, []);
+        var audioCtx = new (window.AudioContext || window.webkitAudioContext)(); // get access to audio context
 
-    function constructPlayer() {
-        const videoElement = document.createElement("video");
+        // Wait for window.onload to fire. See crbug.com/112368
+        // Our <video> element will be the audio source.
+        var source = audioCtx.createMediaElementSource(videoElement);
+        const canvas = canvasRef.current; //config canvas
+        canvas.width = 20;
+        canvas.height = 80;
+        const ctx = canvas.getContext("2d");
 
-        videoElement.classList.add('video-js');
-        videoElement.classList.add('vjs-big-playcentered');
-        videoRef.current.appendChild(videoElement);
-        videoElement.disablepictureinpicture = true;
+        //config audio analyzer
+        const analyser = audioCtx.createAnalyser();
+        analyser.fftSize = 256;
+        const bufferLength = analyser.frequencyBinCount,
+          dataArray = new Uint8Array(bufferLength),
+          WIDTH = canvas.width,
+          HEIGHT = canvas.height,
+          barHeight = (HEIGHT / bufferLength) * 2.5;
+        analyser.getByteTimeDomainData(dataArray);
 
-        const player = playerRef.current = videojs(videoElement, videoJsOptions, () => {
-            videojs.log('player is ready');
-            // You can handle player events here, for example:
+        source.connect(analyser);
+        analyser.connect(audioCtx.destination);
+        let barWidth = 0,
+          y = 0,
+          x = 0;
 
-            var audioCtx = new (window.AudioContext || window.webkitAudioContext)();         // get access to audio context
+        //core logic for the visualizer
+        const timeouts = [];
+        const renderFrame = () => {
+          ctx.fillStyle = "rgba(0,0,0,0)";
+          requestAnimationFrame(renderFrame);
+          y = 0;
+          x = 0;
+          analyser.getByteFrequencyData(dataArray);
+          ctx.clearRect(x, y, WIDTH, HEIGHT);
+          ctx.fillRect(x, y, WIDTH, HEIGHT);
 
-            // Wait for window.onload to fire. See crbug.com/112368
-            // Our <video> element will be the audio source.
-            var source = audioCtx.createMediaElementSource(videoElement);
-            const canvas = canvasRef.current;//config canvas
-            canvas.width = 20;
-            canvas.height = 160;
-            const ctx = canvas.getContext("2d");
+          for (let i = 0; i < bufferLength; i++) {
+            //color based upon frequency
+            barWidth = dataArray[i];
+            let r = barWidth + 22 * (i / bufferLength),
+              g = 333 * (i / bufferLength),
+              b = 47;
+            ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+            ctx.fillRect(x, y, barWidth, barHeight);
+            y += barHeight + 1;
 
-            //config audio analyzer
-            const analyser = audioCtx.createAnalyser();
-            analyser.fftSize = 256;
-            const bufferLength = analyser.frequencyBinCount,
-                dataArray = new Uint8Array(bufferLength),
-                WIDTH = canvas.width,
-                HEIGHT = canvas.height,
-                barHeight = (HEIGHT / bufferLength) * 2.5;
-            analyser.getByteTimeDomainData(dataArray);
+            //Allows visualizer to overlay on a background/video by clearing the rects after painting.
+            // let timer = setTimeout(() => {
+            //     ctx.clearRect(0, 0, WIDTH, HEIGHT);
+            // }, 50);
+            // timeouts.push(timer);
+          }
+        };
+        //Clears the accumulating timeouts.
+        // setTimeout(() => {
+        //     for (let i = 0; i < timeouts.length; i++) {
+        //         return clearTimeout(timeouts[i]);
+        //     }
+        // }, 51);
+        renderFrame();
 
-            source.connect(analyser);
-            analyser.connect(audioCtx.destination);
-            let barWidth = 0,
-                y = 0,
-                x = 0;
-
-            //core logic for the visualizer
-            const timeouts = [];
-            const renderFrame = () => {
-                ctx.fillStyle = "rgba(0,0,0,0)";
-                requestAnimationFrame(renderFrame);
-                y = 0;
-                x = 0;
-                analyser.getByteFrequencyData(dataArray);
-                ctx.clearRect(x, y, WIDTH, HEIGHT);
-                ctx.fillRect(x, y, WIDTH, HEIGHT);
-
-                for (let i = 0; i < bufferLength; i++) {
-                    //color based upon frequency
-                    barWidth = dataArray[i];
-                    let
-                        r = barWidth + 22 * (i / bufferLength),
-                        g = 333 * (i / bufferLength),
-                        b = 47;
-                    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-                    ctx.fillRect(x, y, barWidth , barHeight);
-                    y += barHeight + 1;
-
-                    //Allows visualizer to overlay on a background/video by clearing the rects after painting.
-                    // let timer = setTimeout(() => {
-                    //     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-                    // }, 50);
-                    // timeouts.push(timer);
-                }
-            };
-            //Clears the accumulating timeouts.
-            // setTimeout(() => {
-            //     for (let i = 0; i < timeouts.length; i++) {
-            //         return clearTimeout(timeouts[i]);
-            //     }
-            // }, 51);
-            renderFrame();
-
-
-
-            player.on('waiting', () => {
-                videojs.log('player is waiting');
-            });
-
-            player.on('dispose', () => {
-                videojs.log('player will dispose');
-            });
-            player.on('error', () => {
-                disposePlayer();
-                constructPlayer();
-            });
-            player.on('volumechange', () => {
-                videojs.log('volume changed');
-                videojs.log(player.volume());
-                setVolume(player.volume());
-            });
+        player.on("waiting", () => {
+          videojs.log("player is waiting");
         });
 
-        // You could update an existing player in the `else` block here
-        // on prop change, for example:
-        //set volume
-        player.volume(volume);
-    }
+        player.on("dispose", () => {
+          videojs.log("player will dispose");
+        });
+        player.on("error", (err) => {
+          //setErrorMsg(err);
+          setTimeout(() => {
+            //setErrorMsg(null);
+            disposePlayer();
+            constructPlayer();
+          }, 5000);
+        });
+        player.on("volumechange", () => {
+          videojs.log("volume changed");
+          videojs.log(player.volume());
+          setVolume(player.volume());
+        });
+      }
+    ));
 
-    function disposePlayer() {
-        const player = playerRef.current;
-        if (player && !player.isDisposed()) {
-            player.dispose();
-            playerRef.current = null;
-        }
-    }
-    const handleVolume = (e) => {
-        setVolume(e.target.value);
-    }
-    const handleClose = () => {
-        onClose(video);
-        disposePlayer();
-    };
+    // You could update an existing player in the `else` block here
+    // on prop change, for example:
+    //set volume
+    player.volume(volume);
+  };
 
-    return (
-        <div style={{ width: "400px" }}>
-            <div style={{ display: "flex", padding: "0px 10px", color: "white", textDecoration: "none", wordBreak: 'break-all' }}>
-                <div style={{ flexGrow: 1 }}>{video.title}</div>
-                <div onClick={handleClose}>{"close"}</div>
-            </div>
-            <div style={{ position: "relative"}} ref={videoRef} >
-                <canvas style={{ position: "absolute", zIndex: "1000", left:"10px" }} ref={canvasRef} className="canvas"></canvas>
-            </div>
-            <div style={{ display: 'flex', justifyContent: "center", alignContent: 'baseline', background: "blue" }}>
-                <label style={{ color: "white", fontFamily: "verdana", fontSize: "0.75em" }} htmlFor="volume-control">Volume:</label>
-                <input type="range"
-                    min="0"
-                    max="1"
-                    step="0.1"
-                    value={volume}
-                    onChange={handleVolume} />
-                <span>{Math.round(volume * 100)}</span>
-            </div>
-        </div>
-    )
+  const handleVolume = (e) => {
+    setVolume(e.target.value);
+  };
+  const handleClose = () => {
+    onClose(video);
+    disposePlayer();
+  };
+
+  return (
+    <div style={{ width: "200px" }}>
+      <div
+        style={{
+          display: "flex",
+          padding: "0px 10px",
+          color: "white",
+          textDecoration: "none",
+          wordBreak: "break-all",
+        }}
+      >
+        <div style={{ flexGrow: 1 }}>{video.title}</div>
+        <div onClick={handleClose}>{"close"}</div>
+      </div>
+      <div style={{ position: "relative" }} ref={videoRef}>
+        <canvas
+          style={{ position: "absolute", zIndex: "1000", left: "10px" }}
+          ref={canvasRef}
+        ></canvas>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "baseline",
+          background: "blue",
+        }}
+      >
+        <label
+          style={{ color: "white", fontFamily: "verdana", fontSize: "0.75em" }}
+          htmlFor="volume-control"
+        >
+          Volume:
+        </label>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.1"
+          value={volume}
+          onChange={handleVolume}
+        />
+        <span>{Math.round(volume * 100)}</span>
+      </div>
+    </div>
+  );
 }
 export default React.memo(HLSVideoPlayer);
-
-
